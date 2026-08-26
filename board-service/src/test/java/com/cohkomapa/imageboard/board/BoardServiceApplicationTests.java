aa52df9695168d43a8c08a3d05e55109cace422b
Aleksandr Yarkovskiy <yarkovskiy.aleksandr@gmail.com>
2026-08-26 19:57:38 +0300

    chore: initialize imageboard platform

---
+++ board-service/src/test/java/com/cohkomapa/imageboard/board/BoardServiceApplicationTests.java
@@ -0,0 +1,12 @@
+package com.cohkomapa.imageboard.board;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BoardServiceApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+}
