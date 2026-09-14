22a38f1c46e2e7dfebc21f0665c640c0c5d543ce
Aleksandr Yarkovskiy <yarkovskiy.aleksandr@gmail.com>
2026-09-14 23:09:37 +0300

    fix: fix typo in method MediaMapper.mapToDetailsDto()

--- media-service/src/main/java/com/cohkomapa/imageboard/media/mapper/MediaMapper.java
+++ media-service/src/main/java/com/cohkomapa/imageboard/media/mapper/MediaMapper.java
@@ -7,5 +7,5 @@ import org.mapstruct.Mapper;
 @Mapper(componentModel = "spring")
 public interface MediaMapper {
 
-    MediaDetailsDto mapToDetailsDto(MediaEntity post);
+    MediaDetailsDto mapToDetailsDto(MediaEntity media);
 }
